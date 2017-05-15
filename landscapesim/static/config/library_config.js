a2f0c267f3aca7d9eb95cf9194c6d7ed8157b12a
mikegough <mike.gough@consbio.org>
2017-05-15 12:18:54 -0700

    Added Library Config file

---
+++ landscapesim/static/config/library_config.js
@@ -0,0 +1,10 @@
+library_config = {};
+
+library_config[1] = {
+        name: "Castle Creek",
+        description : "A sample ST-Sim library developed for a semi-arid shrub-steppe ecosystem in southwest Idaho (Castle Creek).",
+        author: "Michael S. O'Donnell",
+        date: "2015",
+        extent: [[42.55560932868032, -116.61197662353517], [42.803209831594415, -116.27449035644533]]
+};
+
