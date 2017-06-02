c60e23838316fd2faf28f3009780925bf095dd77
Taylor Mutch <taylormutch@gmail.com>
2017-06-02 13:45:56 -0700

    Moved tool url into main config.

--- landscapesim_project/urls.py
+++ landscapesim_project/urls.py
@@ -2,19 +2,19 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('landscapesim.urls')),
-    url(r'^maps/', include('ncdjango.urls'))
+    url(r'^maps/', include('ncdjango.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 ]
 
 if settings.DEBUG:
-    from django.views.generic import TemplateView
     urlpatterns += [
         url(r'^basictest/$', TemplateView.as_view(template_name='test.html')),
         url(r'^maptest/$', TemplateView.as_view(template_name='mapviewer.html')),
-        url(r'^$', TemplateView.as_view(template_name='index.html'))
     ]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
