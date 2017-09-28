cd87b4708fed35cb3be13229417fdc5da7781a54
Taylor Mutch <taylormutch@gmail.com>
2017-09-27 20:36:48 -0700

    Minor refactor.

--- landscapesim/static/js/scene.js
+++ landscapesim/static/js/scene.js
@@ -1,5 +1,4 @@
 // Sets up a simple 3D viewer behind the map
-
 var container = document.getElementById('scene');
 
 var topographicBasemapUrl = L.esri.BasemapLayer.TILES.Imagery.urlTemplate.replace('{s}', 'services');
@@ -27,10 +26,9 @@ var getTileUrl = function(template, z, x, y) {
     return template.replace('{z}', z).replace('{x}', x).replace('{y}', y)
 };
 
-
 var metersPerPixel = function(z) {
     return 6378137.0 * 2.0 * Math.PI / (tile_size * Math.pow(2, z));
-}
+};
 
 // num tiles, width and height <--> x and y
 // Increase the number of tiles to see more of the area
@@ -59,99 +57,101 @@ var loader = new THREE.TextureLoader();
 // light uniform
 var dynamicLightPosition = [1.0,0.0,1.0];
 
+var vertexShader = ["attribute vec3 position;",
+        "attribute vec2 uv;",
+        "",
+        "precision mediump float;",
+        "precision mediump int;",
+        "uniform mat4 modelViewMatrix; // optional",
+        "uniform mat4 projectionMatrix; // optional",
+        "uniform float zoom;",
+        "",
+        "uniform sampler2D heightMap;",
+        "uniform sampler2D normalMap;",
+        "uniform vec3 light;",
+        //"uniform float disp;// displacement",
+        "",
+        "varying vec2 vUV;",
+        "varying vec3 fN;",
+        "varying vec3 fE;",
+        "varying vec3 fL;",
+        "",
+        "float mpp()",
+            "{return 6378137.0 * 2.0 * 3.141592653589793 / (256.0 * exp2(zoom));}",
+        "",
+        "float decodeHeight(vec4 texture)",
+        "{",
+        "   return (texture.r * 256.0 * 256.0 + texture.g * 256.0 + texture.b - 32768.0);",
+        "}",
+        "",
+        "void main()",
+        "{",
+        "   vUV = uv;",
+        "   float h = decodeHeight(texture2D(heightMap, uv)) / mpp();",
+        "   fN = normalize(texture2D(normalMap, uv).xyz * 256.);",
+        "   fE = -(modelViewMatrix * vec4(position, 1.0)).xyz;",
+        "   fL = normalize(light);",
+        "   gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, h, position.z, 1.0);",
+        "}"].join('\n');
+var fragmentShader= ["precision mediump float;",
+        "precision mediump int;",
+        "",
+        "uniform sampler2D basemap;",
+        "",
+        "varying vec2 vUV;",
+        "varying vec3 fN;",
+        "varying vec3 fE;",
+        "varying vec3 fL;",
+        "",
+        "vec4 not(vec4 a) {",
+        "  return 1.0 - a;",
+        "}",
+        "",
+        "void main()",
+        "{",
+        "// constant ambient, diffuse and specular and shiny values",
+        "float KA = 0.5;",
+        "float KD = 1.0;",
+        "float KS = 0.15;",
+        "float SHINY = 20.0;",
+        "",
+        "// sample the layer data",
+        "vec4 baseColor = texture2D(basemap, vUV);",
+        "",
+        "// compute lighting per vertex",
+        "vec3 N = normalize(fN);",
+        "    vec3 E = normalize(fE);",
+        "    vec3 L = normalize(fL);",
+        "    vec3 H = normalize( L + E );",
+        "    vec4 ambient = vec4(vec3(KA),1.0)*baseColor;",
+        "    float diffDot = max(dot(L, N), 0.0);",
+        "    vec4 diffuse = diffDot*vec4(vec3(KD),1.0)*baseColor;",
+        "    float specDot = pow(max(dot(N, H), 0.0), SHINY);",
+        "    vec4 specular = specDot*vec4(vec3(KS),1.0)*baseColor;",
+        "",
+        "    // zero the specular highlight if the light is behind the vertex being drawn",
+        "    if( dot(L, N) < 0.0 ) {",
+        "       specular = vec4(0.0, 0.0, 0.0, 1.0);",
+        "    }",
+        "",
+        "    vec4 finalColor = ambient + diffuse + specular;",
+        "    finalColor.a = 1.0;",
+        "gl_FragColor = finalColor;",
+        "}"].join('\n');
+
 function addOneTile(x_offset, y_offset, base_texture, height_texture, normal_texture) {
 	var geometry = new THREE.PlaneBufferGeometry(tile_size, tile_size, tile_size-1, tile_size-1);
 	var material = new THREE.RawShaderMaterial(
 	{
 		uniforms: {
-			't_height': {value: height_texture},
-			't_normal': {value: normal_texture},
-			'b_tex': {value: base_texture},
+			'heightMap': {value: height_texture},
+			'normalMap': {value: normal_texture},
+			'basemap': {value: base_texture},
             'zoom': {value: _zoom},
 			'light': {value: dynamicLightPosition}
 		},
-        vertexShader: "\t\t\tattribute vec3 position;\n" +
-        "\t\t\tattribute vec2 uv;\n" +
-        "\n" +
-        "\t\t\tprecision mediump float;\n" +
-        "\t\t\tprecision mediump int;\n" +
-        "\t\t\tuniform mat4 modelViewMatrix; // optional\n" +
-        "\t\t\tuniform mat4 projectionMatrix; // optional\n" +
-        "\t\t\tuniform float zoom;\n" +
-        "\n" +
-        "\t\t\tuniform sampler2D t_height;\n" +
-        "\t\t\tuniform sampler2D t_normal;\n" +
-        "\t\t\tuniform vec3 light;\n" +
-        //"\t\t\tuniform float disp;\t// displacement\n" +
-        "\n" +
-        "\t\t\tvarying vec2 vUV;\n" +
-        "\t\t\tvarying vec3 fN;\n" +
-        "\t\t\tvarying vec3 fE;\n" +
-        "\t\t\tvarying vec3 fL;\n" +
-        "\n" +
-        "float mpp()\n" +
-            "{return 6378137.0 * 2.0 * 3.141592653589793 / (256.0 * exp2(zoom));}\n" +
-        "\n" +
-        "\t\t\tfloat decodeHeight(vec4 texture)\n" +
-        "\t\t\t{\n" +
-        "\t\t\t\treturn (texture.r * 256.0 * 256.0 + texture.g * 256.0 + texture.b - 32768.0);\n" +
-        "\t\t\t}\n" +
-        "\n" +
-        "\t\t\tvoid main()\n" +
-        "\t\t\t{\n" +
-        "\t\t\t\tvUV = uv;\n" +
-        "\t\t\t\tfloat h = decodeHeight(texture2D(t_height, uv)) / mpp();\n" +
-        "\t\t\t\tfN = normalize(texture2D(t_normal, uv).xyz * 256.);\n" +
-        "\t\t\t\tfE = -(modelViewMatrix * vec4(position, 1.0)).xyz;\n" +
-        "\t\t\t\tfL = normalize(light);\n" +
-        "\t\t\t\tgl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, h, position.z, 1.0);\n" +
-        "\t\t\t}",
-        fragmentShader: "\t\t\tprecision mediump float;\n" +
-        "\t\t\tprecision mediump int;\n" +
-        "\n" +
-        "\t\t\tuniform sampler2D b_tex;\n" +
-        "\n" +
-        "\t\t\tvarying vec2 vUV;\n" +
-        "\t\t\tvarying vec3 fN;\n" +
-        "\t\t\tvarying vec3 fE;\n" +
-        "\t\t\tvarying vec3 fL;\n" +
-        "\n" +
-        "\t\t\tvec4 not(vec4 a) {\n" +
-        "\t\t\t  return 1.0 - a;\n" +
-        "\t\t\t}\n" +
-        "\n" +
-        "\t\t\tvoid main()\n" +
-        "\t\t\t{\n" +
-        "\t\t\t\t// constant ambient, diffuse and specular and shiny values\n" +
-        "\t\t\t\tfloat KA = 0.5;\n" +
-        "\t\t\t\tfloat KD = 1.0;\n" +
-        "\t\t\t\tfloat KS = 0.15;\n" +
-        "\t\t\t\tfloat SHINY = 20.0;\n" +
-        "\n" +
-        "\t\t\t\t// sample the layer data\n" +
-        "\t\t\t\tvec4 baseColor = texture2D(b_tex, vUV);\n" +
-        "\n" +
-        "\t\t\t\t// compute lighting per vertex\n" +
-        "\t\t\t\tvec3 N = normalize(fN);\n" +
-        "\t\t\t    vec3 E = normalize(fE);\n" +
-        "\t\t\t    vec3 L = normalize(fL);\n" +
-        "\t\t\t    vec3 H = normalize( L + E );\n" +
-        "\t\t\t    vec4 ambient = vec4(vec3(KA),1.0)*baseColor;\n" +
-        "\t\t\t    float diffDot = max(dot(L, N), 0.0);\n" +
-        "\t\t\t    vec4 diffuse = diffDot*vec4(vec3(KD),1.0)*baseColor;\n" +
-        "\t\t\t    float specDot = pow(max(dot(N, H), 0.0), SHINY);\n" +
-        "\t\t\t    vec4 specular = specDot*vec4(vec3(KS),1.0)*baseColor;\n" +
-        "\n" +
-        "\t\t\t    // zero the specular highlight if the light is behind the vertex being drawn\n" +
-        "\t\t\t    if( dot(L, N) < 0.0 ) {\n" +
-        "\t\t\t\t   specular = vec4(0.0, 0.0, 0.0, 1.0);\n" +
-        "\t\t\t    }\n" +
-        "\n" +
-        "\t\t\t    vec4 finalColor = ambient + diffuse + specular;\n" +
-        "\t\t\t    finalColor.a = 1.0;\n" +
-        "\t\t\t\tgl_FragColor = finalColor;\n" +
-        "\t\t\t}"
-
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
 	});
 
 	geometry.rotateX(-Math.PI / 2);
@@ -159,7 +159,7 @@ function addOneTile(x_offset, y_offset, base_texture, height_texture, normal_tex
 
 	tile.translateOnAxis(new THREE.Vector3(1,0,0), x_offset);
 	tile.translateOnAxis(new THREE.Vector3(0,0,1), y_offset);
-	scene.add( tile );
+    return tile;
 }
 
 // create one tile mesh
@@ -172,10 +172,12 @@ function createOneTile(x_idx,y_idx, x_offset, y_offset) {
     var normalTileUrl = getTileUrl(normalDataUrl, z, x, y);
     var baseTileUrl = getTileUrl(topographicBasemapUrl, z, x, y);
 
-	loader.load(baseTileUrl, function(b_texture) {
-		loader.load(heightTileUrl, function(h_texture) {
-			loader.load(normalTileUrl, function(t_texture) {
-                addOneTile(x_offset, y_offset, b_texture, h_texture, t_texture);
+	loader.load(baseTileUrl, function(basemap) {
+		loader.load(heightTileUrl, function(height) {
+			loader.load(normalTileUrl, function(normals) {
+                var tile = addOneTile(x_offset, y_offset, basemap, height, normals);
+                tile.userData = {z: z, x: x, y: y};
+                scene.add(tile)
 				num_requests--;
 				if (num_requests === 0) {
 				    console.log(new Date().getTime() - start);
