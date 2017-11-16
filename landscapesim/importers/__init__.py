4e70d8f0c64c2ff8ee1476d506345680f1d3f19c
Taylor Mutch <taylormutch@gmail.com>
2017-11-16 11:11:04 -0800

    Similar technique for importing projects.

--- landscapesim/importers/__init__.py
+++ landscapesim/importers/__init__.py
@@ -1,5 +1,6 @@
 from .scenario import ScenarioImporter
 from .project import ProjectImporter
 from .report import ReportImporter
+from .base import Filter
 
-__all__ = ['ScenarioImporter', 'ProjectImporter', 'ReportImporter']
+__all__ = ['ScenarioImporter', 'ProjectImporter', 'ReportImporter', 'Filter']
--- landscapesim/importers/project.py
+++ landscapesim/importers/project.py
@@ -1,8 +1,94 @@
 import csv
+import os
+from inspect import isfunction
+
+from django.conf import settings
 
 from landscapesim import models
+from landscapesim.io import config
 from landscapesim.io.types import default_int, empty_or_yes_to_bool
 from landscapesim.io.utils import get_random_csv
+from .base import Filter
+
+DEBUG = getattr(settings, 'DEBUG')
+
+
+TransitionTypeFilter = Filter(models.TransitionType)
+TransitionGroupFilter = Filter(models.TransitionGroup)
+AttributeGroupFilter = Filter(models.AttributeGroup)
+
+TERMINOLOGY = (
+    'STSim_Terminology',
+    models.Terminology,
+    config.TERMINOLOGY,
+    (str, str, str, str, str, str, str)
+)
+DISTRIBUTION_TYPE = (
+    'Stats_DistributionType',
+    models.DistributionType,
+    config.DISTRIBUTION_TYPE,
+    (str, str, empty_or_yes_to_bool)
+)
+STRATUM = (
+    'STSim_Stratum',
+    models.Stratum,
+    config.STRATUM,
+    (str, str, str, default_int)
+)
+SECONDARY_STRATUM = (
+    'STSim_SecondaryStratum',
+    models.SecondaryStratum,
+    config.SECONDARY_STRATUM,
+    (str, str, default_int)
+)
+STATECLASS = (
+    'STSim_StateClass',
+    models.StateClass,
+    config.STATECLASS,
+    (str, str, str, str, str, default_int)
+)
+TRANSITION_TYPE = (
+    'STSim_TransitionType',
+    models.TransitionType,
+    config.TRANSITION_TYPE,
+    (str, str, str, default_int)
+)
+TRANSITION_GROUP = (
+    'STSim_TransitionGroup',
+    models.TransitionGroup,
+    config.TRANSITION_GROUP,
+    (str, str)
+)
+TRANSITION_TYPE_GROUP = (
+    'STSim_TransitionTypeGroup',
+    models.TransitionTypeGroup,
+    config.TRANSITION_TYPE_GROUP,
+    (TransitionTypeFilter, TransitionGroupFilter, str)
+)
+TRANSITION_MULTIPLIER_TYPE = (
+    'STSim_TransitionMultiplierType',
+    models.TransitionMultiplierType,
+    config.TRANSITION_MULTIPLIER_TYPE,
+    (str,)
+)
+ATTRIBUTE_GROUP = (
+    'STSim_AttributeGroup',
+    models.AttributeGroup,
+    config.ATTRIBUTE_GROUP,
+    (str, str)
+)
+STATE_ATTRIBUTE_TYPE = (
+    'STSim_StateAttributeType',
+    models.StateAttributeType,
+    config.STATE_ATTRIBUTE_TYPE,
+    (str, str, str, AttributeGroupFilter)
+)
+TRANSITION_ATTRIBUTE_TYPE = (
+    'STSim_StateAttributeType',
+    models.StateAttributeType,
+    config.STATE_ATTRIBUTE_TYPE,
+    (str, str, str, AttributeGroupFilter)
+)
 
 
 class ProjectImporter:
@@ -10,179 +96,73 @@ class ProjectImporter:
     def __init__(self, project, console):
         self.project = project
         self.console = console
+        self.kwargs = {'pid': self.project.pid, 'overwrite': True, 'orig': True}
+        self.temp_file = get_random_csv(self.project.library.tmp_file)
 
-    def _import_table(self, config):
-        pass
+    def map_row(self, row_data, sheet_map, type_map):
+        result = {}
+        for pair, type_or_filter in zip(sheet_map, type_map):
+            model_field, sheet_field = pair
+            data = row_data[sheet_field]
+            is_filter = not (isinstance(type_or_filter, type) or isfunction(type_or_filter))
+            result[model_field] = type_or_filter.get(data, self.project) if is_filter else type_or_filter(data)
+        return result
+
+    def _clean_temp_file(self):
+        if not DEBUG and os.path.exists(self.temp_file):
+            os.remove(self.temp_file)
+
+    def _extract_sheet(self, sheet_config):
+        sheet_name, model, sheet_map, type_map = sheet_config
+        self.console.export_sheet(sheet_name, self.temp_file, **self.kwargs)
+        with open(self.temp_file, 'r') as sheet:
+            reader = csv.DictReader(sheet)
+            for row in reader:
+                model.objects.create(project=self.project, **self.map_row(row, sheet_map, type_map))
+        self._clean_temp_file()
+
+    def import_terminology(self):
+        self._extract_sheet(TERMINOLOGY)
+
+    def import_distribution_types(self):
+        self._extract_sheet(DISTRIBUTION_TYPE)
+
+    def import_stratum(self):
+        self._extract_sheet(STRATUM)
+
+    def import_secondary_stratum(self):
+        self._extract_sheet(SECONDARY_STRATUM)
+
+    def import_stateclasses(self):
+        self._extract_sheet(STATECLASS)
+
+    def import_transition_types(self):
+        self._extract_sheet(TRANSITION_TYPE)
+
+    def import_transition_groups(self):
+        self._extract_sheet(TRANSITION_GROUP)
+
+    def import_transition_multiplier_types(self):
+        self._extract_sheet(TRANSITION_MULTIPLIER_TYPE)
+
+    def import_attribute_groups(self):
+        self._extract_sheet(ATTRIBUTE_GROUP)
+
+    def import_state_attribute_types(self):
+        self._extract_sheet(STATE_ATTRIBUTE_TYPE)
+
+    def import_transition_attribute_types(self):
+        self._extract_sheet(TRANSITION_ATTRIBUTE_TYPE)
 
     def process_project_definitions(self):
-        tmp_file = get_random_csv(self.project.library.tmp_file)
-        kwgs = {'pid': self.project.pid, 'overwrite': True, 'orig': True}
-
-        # Import terminology
-        self.console.export_sheet('STSim_Terminology', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            terms = [r for r in reader][0]
-            models.Terminology.objects.create(
-                project=self.project,
-                amount_label=terms['AmountLabel'],
-                amount_units=terms['AmountUnits'],
-                state_label_x=terms['StateLabelX'],
-                state_label_y=terms['StateLabelY'],
-                primary_stratum_label=terms['PrimaryStratumLabel'],
-                secondary_stratum_label=terms['SecondaryStratumLabel'],
-                timestep_units=terms['TimestepUnits']
-            )
-        print('Imported terminology for project {}.'.format(self.project.name))
-
-        self.console.export_sheet('Stats_DistributionType', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.DistributionType.objects.create(
-                    project=self.project,
-                    name=row['Name'],
-                    description=row['Description'],
-                    is_internal=empty_or_yes_to_bool(row['IsInternal'])
-                )
-        print('Imported distribution types for project {}.'.format(self.project.name))
-
-        # Import strata
-        self.console.export_sheet('STSim_Stratum', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.Stratum.objects.create(
-                    stratum_id=default_int(row['ID']),
-                    project=self.project,
-                    name=row['Name'],
-                    color=row['Color'],
-                    description=row['Description']
-                )
-        print('Imported strata for project {}.'.format(self.project.name))
-
-        # import secondary strata
-        self.console.export_sheet('STSim_SecondaryStratum', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.SecondaryStratum.objects.create(
-                    project=self.project,
-                    secondary_stratum_id=default_int(row['ID']),
-                    name=row['Name'],
-                    description=row['Description']
-                )
-        print('Imported secondary strata for project {}.'.format(self.project.name))
-
-        # import stateclasses
-        self.console.export_sheet('STSim_StateClass', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.StateClass.objects.create(
-                    stateclass_id=default_int(row['ID']),
-                    project=self.project,
-                    name=row['Name'],
-                    state_label_x=row['StateLabelXID'],
-                    state_label_y=row['StateLabelYID'],
-                    color=row['Color'],
-                    description=row['Description']
-                )
-        print('Imported state classes for project {}.'.format(self.project.name))
-
-        # import transition types
-        self.console.export_sheet('STSim_TransitionType', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.TransitionType.objects.create(
-                    project=self.project,
-                    transition_type_id=default_int(row['ID']),
-                    name=row['Name'],
-                    color=row['Color'],
-                    description=row['Description']
-                )
-        print('Imported transition types for project {}.'.format(self.project.name))
-
-        # import transition groups
-        self.console.export_sheet('STSim_TransitionGroup', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.TransitionGroup.objects.create(
-                    project=self.project,
-                    name=row['Name'],
-                    description=row['Description']
-                )
-        print('Imported transition groups for project {}.'.format(self.project.name))
-
-        # map transition groups to transition types
-        self.console.export_sheet('STSim_TransitionTypeGroup', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.TransitionTypeGroup.objects.create(
-                    project=self.project,
-                    transition_type=models.TransitionType.objects.filter(
-                        name__exact=row['TransitionTypeID'], project=self.project
-                    ).first(),
-                    transition_group=models.TransitionGroup.objects.filter(
-                        name__exact=row['TransitionGroupID'], project=self.project
-                    ).first(),
-                    is_primary=row['IsPrimary']
-                )
-        print('Imported transition type groups for project {}.'.format(self.project.name))
-
-        # import transition multiplier types
-        self.console.export_sheet('STSim_TransitionMultiplierType', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.TransitionMultiplierType.objects.create(
-                    project=self.project,
-                    name=row['Name']
-                )
-        print('Imported transition multiplier types for project {}.'.format(self.project.name))
-
-        # Import attribute groups
-        self.console.export_sheet('STSim_AttributeGroup', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.AttributeGroup.objects.create(
-                    project=self.project,
-                    name=row['Name'],
-                    description=row['Description']
-                )
-        print('Imported attribute groups for project {}.'.format(self.project.name))
-
-        # Import state attribute types
-        self.console.export_sheet('STSim_StateAttributeType', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.StateAttributeType.objects.create(
-                    project=self.project,
-                    name=row['Name'],
-                    attribute_group=models.AttributeGroup.objects.filter(name__exact=row['AttributeGroupID'],
-                                                                         project=self.project).first(),
-                    units=row['Units'],
-                    description=row['Description']
-                )
-        print('Imported state attribute types for project {}.'.format(self.project.name))
-
-        # Import transition attribute types
-        self.console.export_sheet('STSim_TransitionAttributeType', tmp_file, **kwgs)
-        with open(tmp_file, 'r') as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                models.TransitionAttributeType.objects.create(
-                    project=self.project,
-                    name=row['Name'],
-                    attribute_group=models.AttributeGroup.objects.filter(name__exact=row['AttributeGroupID'],
-                                                                         project=self.project).first(),
-                    units=row['Units'],
-                    description=row['Description']
-                )
-        print('Imported transition attribute types for project {}.'.format(self.project.name))
-
+        self.import_terminology()
+        self.import_distribution_types()
+        self.import_stratum()
+        self.import_secondary_stratum()
+        self.import_stateclasses()
+        self.import_transition_types()
+        self.import_transition_groups()
+        self.import_transition_multiplier_types()
+        self.import_attribute_groups()
+        self.import_state_attribute_types()
+        self.import_transition_attribute_types()
