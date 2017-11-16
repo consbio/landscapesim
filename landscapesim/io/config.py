5d36c0f820aa1fc4c0c94f426667451e15cbd614
Taylor Mutch <taylormutch@gmail.com>
2017-11-15 22:45:23 -0800

    Code style fixes.

--- landscapesim/io/config.py
+++ landscapesim/io/config.py
@@ -6,6 +6,9 @@
 """
 
 # Commonly used mapping components
+_name = ('name', 'Name')
+_color = ('color', 'Color')
+_description = ('description', 'Description')
 _stratum = ('stratum', 'StratumID')
 _stratum_src = ('stratum_src', 'StratumIDSource')
 _stratum_dest = ('stratum_dest', 'StratumIDDest')
@@ -36,183 +39,206 @@ _age_common = (_age_min, _age_max)
 _distribution_common = (_distribution_type, _distribution_sd, _distribution_min, _distribution_max)
 
 
-DISTRIBUTION_VALUE = (_distribution_type_id,
-                      ('dmin', 'Min'),
-                      ('dmax', 'Max'),
-                      ('relative_frequency', 'RelativeFrequency'))
+# Scenario-specific mappings
+DISTRIBUTION_VALUE = (
+    _distribution_type_id,
+    ('dmin', 'Min'),
+    ('dmax', 'Max'),
+    ('relative_frequency', 'RelativeFrequency')
+)
 
-RUN_CONTROL = (('min_iteration', 'MinimumIteration'),
-               ('max_iteration', 'MaximumIteration'),
-               ('min_timestep', 'MinimumTimestep'),
-               ('max_timestep', 'MaximumTimestep'),
-               ('is_spatial', 'IsSpatial'))
-
-OUTPUT_OPTION = (('sum_sc', 'SummaryOutputSC'),
-                 ('sum_sc_t', 'SummaryOutputSCTimesteps'),
-                 ('sum_sc_zeros', 'SummaryOutputSCZeroValues'),
-                 ('raster_sc', 'RasterOutputSC'),
-                 ('raster_sc_t', 'RasterOutputSCTimesteps'),
-                 ('sum_tr', 'SummaryOutputTR'),
-                 ('sum_tr_t', 'SummaryOutputTRTimesteps'),
-                 ('sum_tr_interval', 'SummaryOutputTRIntervalMean'),
-                 ('raster_tr', 'RasterOutputTR'),
-                 ('raster_tr_t', 'RasterOutputTRTimesteps'),
-                 ('sum_trsc', 'SummaryOutputTRSC'),
-                 ('sum_trsc_t', 'SummaryOutputTRSCTimesteps'),
-                 ('sum_sa', 'SummaryOutputSA'),
-                 ('sum_sa_t', 'SummaryOutputSATimesteps'),
-                 ('raster_sa', 'RasterOutputSA'),
-                 ('raster_sa_t', 'RasterOutputSATimesteps'),
-                 ('sum_ta', 'SummaryOutputTA'),
-                 ('sum_ta_t', 'SummaryOutputTATimesteps'),
-                 ('raster_ta', 'RasterOutputTA'),
-                 ('raster_ta_t', 'RasterOutputTATimesteps'),
-                 ('raster_strata', 'RasterOutputST'),
-                 ('raster_strata_t', 'RasterOutputSTTimesteps'),
-                 ('raster_age', 'RasterOutputAge'),
-                 ('raster_age_t', 'RasterOutputAgeTimesteps'),
-                 ('raster_tst', 'RasterOutputTST'),
-                 ('raster_tst_t', 'RasterOutputTSTTimesteps'),
-                 ('raster_aatp', 'RasterOutputAATP'),
-                 ('raster_aatp_t', 'RasterOutputAATPTimesteps'))
-
-DETERMINISTIC_TRANSITION = (_stratum_src,
-                            _stateclass_src,
-                            _stratum_dest,
-                            _stateclass_dest,
-                            *_age_common,
-                            ('location', 'Location'))
-
-TRANSITION = (_stratum_src,
-              _stateclass_src,
-              _stratum_dest,
-              _stateclass_dest,
-              _transition_type,
-              ('probability', 'Probability'),
-              ('proportion', 'Proportion'),
-              ('age_relative', 'AgeRelative'),
-              ('age_reset', 'AgeReset'),
-              *_age_common,
-              ('tst_min', 'TSTMin'),
-              ('tst_max', 'TSTMax'),
-              ('tst_relative', 'TSTRelative'))
-
-INITIAL_CONDITIONS_NON_SPATIAL = (('total_amount', 'TotalAmount'),
-                                  ('num_cells', 'NumCells'),
-                                  ('calc_from_dist', 'CalcFromDist'))
-
-INITIAL_CONDITIONS_NON_SPATIAL_DISTRIBUTION = (_stratum,
-                                               _secondary_stratum,
-                                               _stateclass,
-                                               ('relative_amount', 'RelativeAmount'),
-                                               *_age_common)
-
-INITIAL_CONDITIONS_SPATIAL = (('num_rows', 'NumRows'),
-                              ('num_cols', 'NumColumns'),
-                              ('num_cells', 'NumCells'),
-                              ('cell_size', 'CellSize'),
-                              ('cell_size_units', 'CellSizeUnits'),
-                              ('cell_area', 'CellArea'),
-                              ('cell_area_override', 'CellAreaOverride'),
-                              ('xll_corner', 'XLLCorner'),
-                              ('yll_corner', 'YLLCorner'),
-                              ('srs', 'SRS'),
-                              ('stratum_file_name', 'StratumFileName'),
-                              ('secondary_stratum_file_name', 'SecondaryStratumFileName'),
-                              ('stateclass_file_name', 'StateClassFileName'),
-                              ('age_file_name', 'AgeFileName'))
-
-TRANSITION_TARGET = (*_time_common,
-                     _stratum,
-                     _secondary_stratum,
-                     _transition_group,
-                     ('target_area', 'Amount'),
-                     *_distribution_common)
-
-TRANSITION_MULTIPLIER_VALUE = (*_time_common,
-                               _stratum,
-                               _secondary_stratum,
-                               _stateclass,
-                               _transition_group,
-                               _transition_multiplier_type,
-                               ('multiplier', 'Amount'),
-                               *_distribution_common)
-
-TRANSITION_SIZE_DISTRIBUTION = (*_time_common,
-                                _stratum,
-                                _transition_group,
-                                ('relative_amount', 'RelativeAmount'))
-
-TRANSITION_SIZE_PRIORITIZATION = (*_time_common,
-                                  _stratum,
-                                  _transition_group,
-                                  ('priority', 'Priority'))
-
-TRANSITION_SPATIAL_MULTIPLIER = (*_time_common,
-                                 _transition_group,
-                                 _transition_multiplier_type,
-                                 ('transition_multiplier_file_name', 'MultiplierFileName'))
-
-STATE_ATTRIBUTE_VALUE = (*_time_common,
-                         _stratum,
-                         _secondary_stratum,
-                         _stateclass,
-                         _state_attribute_type,
-                         ('value', 'Value'))
-
-TRANSITION_ATTRIBUTE_VALUE = (*_time_common,
-                              _stratum,
-                              _secondary_stratum,
-                              _transition_group,
-                              _stateclass,
-                              _transition_attribute_type,
-                              ('value', 'Value'))
-
-TRANSITION_ATTRIBUTE_TARGET = (*_time_common,
-                               _stratum,
-                               _secondary_stratum,
-                               _transition_attribute_type,
-                               ('target', 'Amount'),
-                               *_distribution_common)
-
-STATECLASS_SUMMARY_ROW = (*_time_common,
-                          _stratum,
-                          _stateclass,
-                          _amount,
-                          *_age_common,
-                          ('proportion_of_landscape', 'ProportionOfLandscape'),
-                          ('proportion_of_stratum', 'ProportionOfStratumID'),
-                          _secondary_stratum)
-
-TRANSITION_SUMMARY_ROW = (*_time_common,
-                          _stratum,
-                          _transition_group,
-                          *_age_common,
-                          _amount,
-                          _secondary_stratum)
-
-TRANSITION_STATECLASS_SUMMARY_ROW = (*_time_common,
-                                     _stratum,
-                                     _transition_type,
-                                     _stateclass,
-                                     _stateclass_dest_end,
-                                     _amount,
-                                     _secondary_stratum)
-
-STATE_ATTRIBUTE_SUMMARY_ROW = (*_time_common,
-                               _stratum,
-                               _state_attribute_type,
-                               *_age_common,
-                               _amount,
-                               _secondary_stratum)
-
-TRANSITION_ATTRIBUTE_SUMMARY_ROW = (*_time_common,
-                                    _stratum,
-                                    _transition_attribute_type,
-                                    *_age_common,
-                                    _amount,
-                                    _secondary_stratum)
-
+RUN_CONTROL = (
+    ('min_iteration', 'MinimumIteration'),
+    ('max_iteration', 'MaximumIteration'),
+    ('min_timestep', 'MinimumTimestep'),
+    ('max_timestep', 'MaximumTimestep'),
+    ('is_spatial', 'IsSpatial')
+)
+OUTPUT_OPTION = (
+    ('sum_sc', 'SummaryOutputSC'),
+    ('sum_sc_t', 'SummaryOutputSCTimesteps'),
+    ('sum_sc_zeros', 'SummaryOutputSCZeroValues'),
+    ('raster_sc', 'RasterOutputSC'),
+    ('raster_sc_t', 'RasterOutputSCTimesteps'),
+    ('sum_tr', 'SummaryOutputTR'),
+    ('sum_tr_t', 'SummaryOutputTRTimesteps'),
+    ('sum_tr_interval', 'SummaryOutputTRIntervalMean'),
+    ('raster_tr', 'RasterOutputTR'),
+    ('raster_tr_t', 'RasterOutputTRTimesteps'),
+    ('sum_trsc', 'SummaryOutputTRSC'),
+    ('sum_trsc_t', 'SummaryOutputTRSCTimesteps'),
+    ('sum_sa', 'SummaryOutputSA'),
+    ('sum_sa_t', 'SummaryOutputSATimesteps'),
+    ('raster_sa', 'RasterOutputSA'),
+    ('raster_sa_t', 'RasterOutputSATimesteps'),
+    ('sum_ta', 'SummaryOutputTA'),
+    ('sum_ta_t', 'SummaryOutputTATimesteps'),
+    ('raster_ta', 'RasterOutputTA'),
+    ('raster_ta_t', 'RasterOutputTATimesteps'),
+    ('raster_strata', 'RasterOutputST'),
+    ('raster_strata_t', 'RasterOutputSTTimesteps'),
+    ('raster_age', 'RasterOutputAge'),
+    ('raster_age_t', 'RasterOutputAgeTimesteps'),
+    ('raster_tst', 'RasterOutputTST'),
+    ('raster_tst_t', 'RasterOutputTSTTimesteps'),
+    ('raster_aatp', 'RasterOutputAATP'),
+    ('raster_aatp_t', 'RasterOutputAATPTimesteps')
+)
+DETERMINISTIC_TRANSITION = (
+    _stratum_src,
+    _stateclass_src,
+    _stratum_dest,
+    _stateclass_dest,
+    *_age_common,
+    ('location', 'Location')
+)
+TRANSITION = (
+    _stratum_src,
+    _stateclass_src,
+    _stratum_dest,
+    _stateclass_dest,
+    _transition_type,
+    ('probability', 'Probability'),
+    ('proportion', 'Proportion'),
+    ('age_relative', 'AgeRelative'),
+    ('age_reset', 'AgeReset'),
+    *_age_common,
+    ('tst_min', 'TSTMin'),
+    ('tst_max', 'TSTMax'),
+    ('tst_relative', 'TSTRelative')
+)
+INITIAL_CONDITIONS_NON_SPATIAL = (
+    ('total_amount', 'TotalAmount'),
+    ('num_cells', 'NumCells'),
+    ('calc_from_dist', 'CalcFromDist')
+)
+INITIAL_CONDITIONS_NON_SPATIAL_DISTRIBUTION = (
+    _stratum,
+    _secondary_stratum,
+    _stateclass,
+    ('relative_amount', 'RelativeAmount'),
+    *_age_common
+)
+INITIAL_CONDITIONS_SPATIAL = (
+    ('num_rows', 'NumRows'),
+    ('num_cols', 'NumColumns'),
+    ('num_cells', 'NumCells'),
+    ('cell_size', 'CellSize'),
+    ('cell_size_units', 'CellSizeUnits'),
+    ('cell_area', 'CellArea'),
+    ('cell_area_override', 'CellAreaOverride'),
+    ('xll_corner', 'XLLCorner'),
+    ('yll_corner', 'YLLCorner'),
+    ('srs', 'SRS'),
+    ('stratum_file_name', 'StratumFileName'),
+    ('secondary_stratum_file_name', 'SecondaryStratumFileName'),
+    ('stateclass_file_name', 'StateClassFileName'),
+    ('age_file_name', 'AgeFileName')
+)
+TRANSITION_TARGET = (
+    *_time_common,
+    _stratum,
+    _secondary_stratum,
+    _transition_group,
+    ('target_area', 'Amount'),
+    *_distribution_common
+)
+TRANSITION_MULTIPLIER_VALUE = (
+    *_time_common,
+    _stratum,
+    _secondary_stratum,
+    _stateclass,
+    _transition_group,
+    _transition_multiplier_type,
+    ('multiplier', 'Amount'),
+    *_distribution_common
+)
+TRANSITION_SIZE_DISTRIBUTION = (
+    *_time_common,
+    _stratum,
+    _transition_group,
+    ('relative_amount', 'RelativeAmount')
+)
+TRANSITION_SIZE_PRIORITIZATION = (
+    *_time_common,
+    _stratum,
+    _transition_group,
+    ('priority', 'Priority')
+)
+TRANSITION_SPATIAL_MULTIPLIER = (
+    *_time_common,
+    _transition_group,
+    _transition_multiplier_type,
+    ('transition_multiplier_file_name', 'MultiplierFileName')
+)
+STATE_ATTRIBUTE_VALUE = (
+    *_time_common,
+    _stratum,
+    _secondary_stratum,
+    _stateclass,
+    _state_attribute_type,
+    ('value', 'Value')
+)
+TRANSITION_ATTRIBUTE_VALUE = (
+    *_time_common,
+    _stratum,
+    _secondary_stratum,
+    _transition_group,
+    _stateclass,
+    _transition_attribute_type,
+    ('value', 'Value')
+)
+TRANSITION_ATTRIBUTE_TARGET = (
+    *_time_common,
+    _stratum,
+    _secondary_stratum,
+    _transition_attribute_type,
+    ('target', 'Amount'),
+    *_distribution_common
+)
+STATECLASS_SUMMARY_ROW = (
+    *_time_common,
+    _stratum,
+    _stateclass,
+    _amount,
+    *_age_common,
+    ('proportion_of_landscape', 'ProportionOfLandscape'),
+    ('proportion_of_stratum', 'ProportionOfStratumID'),
+    _secondary_stratum
+)
+TRANSITION_SUMMARY_ROW = (
+    *_time_common,
+    _stratum,
+    _transition_group,
+    *_age_common,
+    _amount,
+    _secondary_stratum
+)
+TRANSITION_STATECLASS_SUMMARY_ROW = (
+    *_time_common,
+    _stratum,
+    _transition_type,
+    _stateclass,
+    _stateclass_dest_end,
+    _amount,
+    _secondary_stratum
+)
+STATE_ATTRIBUTE_SUMMARY_ROW = (
+    *_time_common,
+    _stratum,
+    _state_attribute_type,
+    *_age_common,
+    _amount,
+    _secondary_stratum
+)
+TRANSITION_ATTRIBUTE_SUMMARY_ROW = (
+    *_time_common,
+    _stratum,
+    _transition_attribute_type,
+    *_age_common,
+    _amount,
+    _secondary_stratum
+)
 # Configuration of run outputs, time control, distributions, etc.
 CONFIG_IMPORTS = (
     ('run_control', 'STSim_RunControl', RUN_CONTROL),
@@ -220,7 +246,6 @@ CONFIG_IMPORTS = (
     ('initial_conditions_nonspatial_settings', 'STSim_InitialConditionsNonSpatial', INITIAL_CONDITIONS_NON_SPATIAL),
     #('initial_conditions_spatial_settings', 'STSim_InitialConditionsSpatial', INITIAL_CONDITIONS_SPATIAL)
 )
-
 # Configuration of input data (probabilities, mappings, etc.)
 VALUE_IMPORTS = (
     ('deterministic_transitions', 'STSim_DeterministicTransition', DETERMINISTIC_TRANSITION),
